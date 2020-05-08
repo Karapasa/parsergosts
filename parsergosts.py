9d65bea52e99ae5af595dab3d8666a9ae79ee268
Karapasa <alexmixpetrov@gmail.com>
2020-05-08 20:28:03 +0300

    рефактор

--- parsergosts.py
+++ parsergosts.py
@@ -7,20 +7,24 @@ start_url = 'https://internet-law.ru/gosts/3755'
 MAIN_URL = 'https://internet-law.ru/gosts/'
 DOMEN = 'https://internet-law.ru'
 
-set_urls=[start_url]
-visited_urls =[]
+set_urls = [start_url]
+visited_urls = []
 
 result = []
 
+
 def get_page(url):
     session = requests.Session()
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'}
     try:
         page = session.get(url, headers=headers)
         page.encoding = 'windows-1251'
         return page.text
     except:
         print("СТРАНИЦА {} НЕДОСТУПНА!!!".format(url))
+
 
 def parsing(set_urls):
     global visited_urls
@@ -45,12 +49,13 @@ def parsing(set_urls):
 def get_links(soup, url):
     global set_urls
     try:
-        for link in soup.find('div', id='p'+get_url_id(url)).next_sibling.find_all('a'):
+        for link in soup.find('div', id='p' + get_url_id(url)).next_sibling.find_all('a'):
             if 'href' in link.attrs:
                 if get_full_url_for_spider(link.attrs['href'].rstrip('/')) not in set_urls:
                     set_urls.append(get_full_url_for_spider(link.attrs['href'].rstrip('/')))
     except:
         print('На странице {} ссылок НЕТ'.format(url))
+
 
 def check_parsing_table(soup):
     if soup.find('table', class_='ListGost'):
@@ -71,24 +76,25 @@ def parsing_all_tables(soup, url):
 
 
 def parsing_inner_table(soup, url):
-# Собирает данные из таблицы в словарь
-        try:
-            main_table = soup.find('table', class_='ListGost').tbody
-            res = main_table.find_all('td', class_='c4')
-            parsing = {}
-            for data in res:
-                if data.string == 'действующий':
-                    childs = data.find_previous_siblings('td')
-                    parsing[childs[2].string]=childs[1].string
-            return parsing
-        except:
-            print('Для страницы {} не удалось получить данные таблицы'.format(url))
+    # Собирает данные из таблицы в словарь
+    try:
+        main_table = soup.find('table', class_='ListGost').tbody
+        res = main_table.find_all('td', class_='c4')
+        parsing = {}
+        for data in res:
+            if data.string == 'действующий':
+                childs = data.find_previous_siblings('td')
+                parsing[childs[2].string] = childs[1].string
+        return parsing
+    except:
+        print('Для страницы {} не удалось получить данные таблицы'.format(url))
+
 
 def get_parents(soup, url):
-# Вытащить всех родителей из каталога гостов
+    # Вытащить всех родителей из каталога гостов
     try:
         pick = soup.find('div', id='CatList')
-        block = soup.find('div', id='p'+get_url_id(url))
+        block = soup.find('div', id='p' + get_url_id(url))
         parents = []
         while block.parent != pick:
             parents.append(block.parent.previous_element.string)
@@ -97,36 +103,43 @@ def get_parents(soup, url):
     except:
         print('Для страницы {} не удалост получить родителей'.format(url))
 
+
 def category_title(soup):
-# Для подзаголовка в json
-    cat_title=soup.title
+    # Для подзаголовка в json
+    cat_title = soup.title
     return cat_title.string
 
+
 def len_inner_page(soup):
-# Найти кол-во внутренних страниц в основной таблице гостов
+    # Найти кол-во внутренних страниц в основной таблице гостов
     try:
         inner_block = soup.find('table', class_="Links").find_all('td')
-        return len(inner_block)-1
+        return len(inner_block) - 1
     except:
         return False
 
+
 def get_url_id(url):
-    base_name = url.replace('https://','').split('/')
+    base_name = url.replace('https://', '').split('/')
     return base_name[-1]
 
+
 def get_inner_url(soup, url):
-# Генерация списка url для таблиц с пагинацией
-    list_inner_url=[]
+    # Генерация списка url для таблиц с пагинацией
+    list_inner_url = []
     for i in range(len_inner_page(soup)):
         modurl = MAIN_URL + get_url_id(url) + "/?p=" + str(i)
         list_inner_url.append(modurl)
     return list_inner_url
 
+
 def get_full_url_for_parser(id):
     return MAIN_URL + id
 
+
 def get_full_url_for_spider(link):
     return DOMEN + link
+
 
 parsing(set_urls)
 print(result)
